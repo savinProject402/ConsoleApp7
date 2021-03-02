65359e68775a35d041fb4a169b92b24e131aa4a9
Savin Oleh <savin.project402@gmail.com>
2021-03-02 05:24:18 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp7/ClassList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+   public class ClassList<Person> : IEnumerable
+        where Person : IComparable<Person>
+    {
+        private Person[] people = new Person[' '];
+        private int count = 0;
+
+        public int Count => this.count;
+        public void Add(Person item)
+        {
+            if (this.count == this.people.Length)
+            {
+                Array.Resize(ref this.people, this.people.Length + 1);
+            }
+
+            this.people[this.count] = item;
+            this.count++;
+        }
+        public void AddRange(ICollection<Person> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Add(item);
+            }
+        }
+
+
+
+        public int IndexOf(Person item)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                if (this.people[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        public void Insert(int index, Person item)
+        {
+            if (index < 0 || index >= this.count)
+            {
+                return;
+            }
+
+            var arr = new Person[this.people.Length + 1];
+            Array.Copy(this.people, arr, index);
+            arr[index] = item;
+
+            for (int i = index; i < this.people.Length; i++)
+            {
+                arr[i + 1] = this.people[i];
+            }
+
+            this.people = arr;
+        }
+        public bool Remove(Person item)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                if (this.people[i].Equals(item))
+                {
+                    var arr = new Person[this.people.Length - 1];
+                    Array.Copy(this.people, arr, i);
+                    for (int j = i; i < arr.Length; i++)
+                    {
+                        arr[i] = this.people[i + 1];
+                    }
+
+                    this.people = arr;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index <= this.count && index > -1)
+            {
+                var arr = new Person[index];
+                Array.Copy(this.people, arr, index);
+                this.people = arr;
+                this.count = arr.Length;
+            }
+        }
+        public void Sort()
+        {
+            Array.Sort(this.people);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.people.GetEnumerator();
+        }
+    }
+}
---
+++ ConsoleApp7/Starter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+   public class Starter
+    {
+        public static void Run()
+        {
+
+
+            var a_List = new ClassList<string>();
+            Console.WriteLine("void Add");
+            a_List.Add("Homer");
+            a_List.Add("Marge");
+            foreach (var item in a_List)
+            { Console.Write(item); }
+
+
+            Console.WriteLine();
+            Console.WriteLine("void AddRange");
+
+            var b_List = new List<string>();
+            b_List.Add("Meggie");
+            a_List.AddRange(b_List);
+            foreach (var item in a_List)
+            { Console.Write(item); }
+
+            Console.WriteLine();
+            Console.WriteLine("int IndexOf(T item)");
+            Console.WriteLine(a_List.IndexOf("Meggie"));
+
+            a_List.Remove("o");
+            foreach (var item in a_List)
+            { Console.Write(item); }
+            Console.WriteLine();
+            Console.WriteLine("void Sort()");
+            a_List.Sort();
+            foreach (var item in a_List)
+            { Console.Write(item); }
+
+            Console.WriteLine();
+            Console.WriteLine("void RemoveAt(int index):");
+            a_List.RemoveAt(1);
+            foreach (var item in a_List)
+            { Console.Write(item); }
+            Console.WriteLine();
+            Console.WriteLine("void Insert(int index, T item):");
+            a_List.Insert(2, "Gralsburg");
+            foreach (var item in a_List)
+            { Console.Write(item); }
+
+        }
+    }
+}
